5cca358701036a9896c8e6f94829f074307e2069
InitSync <benjaalejandro206@gmail.com>
2023-08-25 20:26:28 -0400

    feat: Store plugin constant fields

---
+++ plugin/src/main/java/net/velex/commanditems/plugin/Constants.java
@@ -0,0 +1,8 @@
+package net.velex.commanditems.plugin;
+
+public class Constants {
+  public static final String VERSION = "{version}";
+  public static final String ADMIN_COMMAND_PERMISSION = "commanditems.admin";
+  public static final String INSPECT_COMMAND_PERMISSION = "commanditems.inspect";
+  public static final String GIVE_COMMAND_PERMISSION = "commanditems.give";
+}
