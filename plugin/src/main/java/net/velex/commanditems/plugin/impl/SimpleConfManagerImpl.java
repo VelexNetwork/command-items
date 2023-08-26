b1879867db0ef5bf3e7dbf900f436b30fe4f5b40
InitSync <benjaalejandro206@gmail.com>
2023-08-25 20:24:12 -0400

    chore(conf-manager): Initialize `ConfigurationHelper` objects in constructor.

--- plugin/src/main/java/net/velex/commanditems/plugin/impl/SimpleConfManagerImpl.java
+++ plugin/src/main/java/net/velex/commanditems/plugin/impl/SimpleConfManagerImpl.java
@@ -4,8 +4,13 @@ import net.velex.commanditems.api.model.ConfManagerModel;
 import net.velex.commanditems.api.model.config.ConfModel;
 import net.velex.commanditems.api.model.config.MessagesModel;
 import org.jetbrains.annotations.NotNull;
+import space.arim.dazzleconf.ConfigurationOptions;
 import space.arim.dazzleconf.error.InvalidConfigException;
+import space.arim.dazzleconf.ext.snakeyaml.CommentMode;
+import space.arim.dazzleconf.ext.snakeyaml.SnakeYamlConfigurationFactory;
+import space.arim.dazzleconf.ext.snakeyaml.SnakeYamlOptions;
 import space.arim.dazzleconf.helper.ConfigurationHelper;
+import space.arim.dazzleconf.sorter.AnnotationBasedSorter;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -18,7 +23,15 @@ public class SimpleConfManagerImpl implements ConfManagerModel {
   private MessagesModel messagesModel;
   
   public SimpleConfManagerImpl(final @NotNull Path dataFolder) {
-  
+    final var sorter = new ConfigurationOptions.Builder()
+      .sorter(new AnnotationBasedSorter())
+      .build();
+    final var commentMode = new SnakeYamlOptions.Builder()
+      .commentMode(CommentMode.fullComments())
+      .build();
+    
+    confHelper = new ConfigurationHelper<>(dataFolder, "config.yml", SnakeYamlConfigurationFactory.create(ConfModel.class, sorter, commentMode));
+    messagesHelper = new ConfigurationHelper<>(dataFolder, "messages.yml", SnakeYamlConfigurationFactory.create(MessagesModel.class, sorter, commentMode));
   }
   
   @Override
